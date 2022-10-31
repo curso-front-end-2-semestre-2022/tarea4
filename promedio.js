719a374e31dbccf404f80126273128d004a513b5
mirkosilva <mirko.silva.b@gmail.com>
2022-10-31 00:06:24 -0300

    Tarea 4 v1

---
+++ promedio.js
@@ -0,0 +1,22 @@
+const notas = [6,8,9,2,5,10];
+
+function sumaNotas(notas){
+    let suma = 0
+
+        for(let i = 0; i<notas.length; i++){
+            const element = notas[i];
+            suma = element + suma
+        }
+        return suma
+}
+
+//console.log(sumaNotas(notas))
+
+function promedioNotas(notas){
+    //console.log(notas)
+    //console.log(sumaNotas(notas))
+    let promedio = 0
+    promedio = sumaNotas(notas)/(notas.length)
+    return promedio
+}
+console.log(promedioNotas(notas))
